404a1692aa567acaeba33dd3d8b74bc461d5e2b0
wanghongqi <454407725@qq.com>
2016-11-18 14:41:38 +0800

    IU-2016.2.5 <wanghongqi@TN-00-50000958 Create baseRefactoring.xml, markdown.xml, IntelliLang.xml, ide.general.xml, git.xml, project.default.xml, diff.xml, other.xml, applicationLibraries.xml, packages.xml, ignore.xml, find.xml, ui.lnf.xml, File Header.java, filetypes.xml, keymap.xml, javaeeExternalResources.xml, gradle.run.settings.xml, code.style.schemes.xml, plugin_ui.xml, colors.scheme.xml, databaseDrivers.xml, mavenVersion.xml, Ladies Night 2.icls, conda_packages.xml, Default.xml, laf.xml, jdk.table.xml, debugger.renderers.xml, path.macros.xml, Default _1_.xml, web-browsers.xml, vcs.xml, grepConsole.xml, databaseSettings.xml, Darcula_0.icls, mybatis.xml, editor.xml, debugger.xml

---
+++ fileTemplates/includes/File Header.java
@@ -0,0 +1,12 @@
+/**
+ *
+ * <p/>
+ * Copyright: Copyright (c) ${DATE}
+ * <p/>
+ * Company: 途牛科技有限公司华东分公司
+ * <p/>
+ *
+ * @project ${PROJECT_NAME}
+ * @author  ${USER}@tuniu.com
+ * @version 1.0.0
+ */
